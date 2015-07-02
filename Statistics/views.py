6f9d608b94ea7a8e5f56c20c9b1ae5682cf43181
Rémy Lapeyrade <remy@lapeyrade.net>
2015-07-02 17:59:35 +0200

    Create restricted stats view

--- Statistics/views.py
+++ Statistics/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.edit import FormView
 from Statistics.forge import forgeData
-from Statistics.forms import FluxSelectionForm
+from Statistics.forms import FluxSelectionForm, RestrictedFluxSelectionForm
 from Statistics.JSONResponseMixin import JSONResponseMixin
 
 # Create your views here.
@@ -11,7 +11,17 @@ class StatsMembersList(FormView, JSONResponseMixin):
     form_class = FluxSelectionForm
 
     def form_valid(self, form):
+        forge = forgeData()
+        data = forge.get_data(form.get_source(), form.get_destination(), form.get_type(), form.get_period(), form.get_unit())
 
-        data = forgeData.get_data(form.get_source(), form.get_destination(), form.get_type(), form.get_period(), form.get_unit())
+        return JSONResponseMixin.render_to_response(self, data)
+
+class RestrictedStats(FormView, JSONResponseMixin):
+    template_name = 'restricted_chart.html'
+    form_class = RestrictedFluxSelectionForm
+
+    def form_valid(self, form):
+        forge = forgeData()
+        data = forge.get_data('0', '0', form.get_type(), form.get_period(), form.get_unit())
 
         return JSONResponseMixin.render_to_response(self, data)
