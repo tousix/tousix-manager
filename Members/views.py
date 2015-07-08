add1c1a4c0789627e04ea25a4cde8cfb434c789c
Rémy Lapeyrade <remy@lapeyrade.net>
2015-07-08 17:01:24 +0200

    Add  user modification forms (only display works atm)

--- Members/views.py
+++ Members/views.py
@@ -1,8 +1,11 @@
 # -*- coding: utf8 -*-
 __author__ = 'remy'
 from formtools.wizard.views import CookieWizardView
+from django.views.generic.edit import UpdateView
 from Members.forms import MemberForm, BillingForm, TechnicalForm, NOCForm, RouterForm, UserForm
-
+from Authentication.LoginMixin import LoginRequiredMixin
+from database.models import Membre, Hote
+from django.contrib.auth.forms import PasswordChangeForm
 
 class CreateMemberView(CookieWizardView):
 
@@ -29,3 +32,41 @@ class CreateMemberView(CookieWizardView):
         router = form_dict['4'].save(commit=False)
         router.idmembre_id = member.pk
         router.save()
+
+class UpdateMemberView(LoginRequiredMixin, UpdateView):
+    model = Membre
+    form_class = MemberForm
+    template_name = "update_member.html"
+    success_url = "/member/update"
+
+    def get_object(self, queryset=None):
+        return Membre.objects.filter(user=self.request.user).first()
+
+    def get_context_data(self, **kwargs):
+        context = super(UpdateMemberView, self).get_context_data(**kwargs)
+        membre = self.get_object()
+
+        if membre.technical is None:
+            context['technical'] = TechnicalForm(prefix="technical")
+        else:
+            context['technical'] = TechnicalForm(instance=membre.technical, prefix="technical")
+        if membre.noc is None:
+            context['noc'] = NOCForm(prefix="noc")
+        else:
+            context['noc'] = NOCForm(instance=membre.noc, prefix="noc")
+        if membre.billing is None:
+            context['billing'] = BillingForm(prefix="billing")
+        else:
+            context['billing'] = BillingForm(instance=membre.billing, prefix="billing")
+        hosts = Hote.objects.filter(idmembre=membre)
+        context['hosts'] = []
+        i = 0
+        for host in hosts:
+            context['hosts'].append(RouterForm(instance=host, prefix="host"+str(i)))
+            i += 1
+        context['user'] = PasswordChangeForm(user=self.request.user, prefix="user")
+        return context
+
+    def post(self, request, *args, **kwargs):
+        print("test")
+        return super(UpdateMemberView, self).post(request, *args, **kwargs)
