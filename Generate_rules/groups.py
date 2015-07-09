82338a8ed8b573163e3e8fdf82f9dc092b3c8d69
Rémy Lapeyrade <remy@lapeyrade.net>
2015-07-09 10:19:42 +0200

    Format group entry PEP8 style

--- Generate_rules/groups.py
+++ Generate_rules/groups.py
@@ -3,16 +3,89 @@ __author__ = 'remy'
 
 
 class groups(object):
-    groups = {2221123978211977908: [
-                  {"dpid": 2221123978211977908,"type": "FF","group_id": 2,"buckets": [{"watch_port": 51, "watch_group": 4294967295, "weight": 1,"actions": [{"type": "OUTPUT","port": 51}]},{"watch_port": 53, "watch_group": 4294967295, "weight": 1,"actions": [{"type": "OUTPUT","port": 53}]}]},
-                  {"dpid": 2221123978211977908,"type": "FF","group_id": 3,"buckets": [{"watch_port": 53, "watch_group": 4294967295, "weight": 1,"actions": [{"type": "OUTPUT","port": 53}]},{"watch_port": 51, "watch_group": 4294967295, "weight": 1,"actions": [{"type": "OUTPUT","port": 51}]}]}
-    ],
-              16690345156023249453: [
-                    {"dpid": 16690345156023249453,"type": "FF","group_id": 1,"buckets": [{"watch_port": 49, "watch_group": 4294967295, "weight": 1,"actions": [{"type": "OUTPUT","port": 49}]},{"watch_port": 50, "watch_group": 4294967295, "weight": 1,"actions": [{"type": "OUTPUT","port": 50}]}]},
-                    {"dpid": 16690345156023249453,"type": "FF","group_id": 3,"buckets": [{"watch_port": 49, "watch_group": 4294967295, "weight": 1,"actions": [{"type": "OUTPUT","port": 49}]},{"watch_port": 50, "watch_group": 4294967295, "weight": 1,"actions": [{"type": "OUTPUT","port": 50}]}]}
-                ],
-              10925425496585651080: [
-                  {"dpid": 10925425496585651080,"type": "FF","group_id": 1,"buckets": [{"watch_port": 53, "watch_group": 4294967295, "weight": 1,"actions": [{"type": "OUTPUT","port": 53}]},{"watch_port": 51, "watch_group": 4294967295, "weight": 1,"actions": [{"type": "OUTPUT","port": 51}]}]},
-                  {"dpid": 10925425496585651080,"type": "FF","group_id": 2,"buckets": [{"watch_port": 53, "watch_group": 4294967295, "weight": 1,"actions": [{"type": "OUTPUT","port": 53}]},{"watch_port": 51, "watch_group": 4294967295, "weight": 1,"actions": [{"type": "OUTPUT","port": 51}]}]}
-              ]
+    groups = {
+        2221123978211977908: [
+            {"dpid": 2221123978211977908,
+             "type": "FF",
+             "group_id": 2,
+             "buckets": [{"watch_port": 51,
+                          "watch_group": 4294967295,
+                          "weight": 1,
+                          "actions": [{"type": "OUTPUT",
+                                       "port": 51}]},
+                         {"watch_port": 53,
+                          "watch_group": 4294967295,
+                          "weight": 1,
+                          "actions": [{"type": "OUTPUT",
+                                       "port": 53}]}]},
+            {"dpid": 2221123978211977908,
+             "type": "FF",
+             "group_id": 3,
+             "buckets": [{"watch_port": 53,
+                          "watch_group": 4294967295,
+                          "weight": 1,
+                          "actions": [{"type": "OUTPUT",
+                                       "port": 53}]},
+                         {"watch_port": 51,
+                          "watch_group": 4294967295,
+                          "weight": 1,
+                          "actions": [{"type": "OUTPUT",
+                                       "port": 51}]}]}
+        ],
+        16690345156023249453: [
+            {"dpid": 16690345156023249453,
+             "type": "FF",
+             "group_id": 1,
+             "buckets": [{"watch_port": 49,
+                          "watch_group": 4294967295,
+                          "weight": 1,
+                          "actions": [{"type": "OUTPUT",
+                                       "port": 49}]},
+                         {"watch_port": 50,
+                          "watch_group": 4294967295,
+                          "weight": 1,
+                          "actions": [{"type": "OUTPUT",
+                                       "port": 50}]}]},
+            {"dpid": 16690345156023249453,
+             "type": "FF",
+             "group_id": 3,
+             "buckets": [{"watch_port": 49,
+                          "watch_group": 4294967295,
+                          "weight": 1,
+                          "actions": [{"type": "OUTPUT",
+                                       "port": 49}]},
+                         {"watch_port": 50,
+                          "watch_group": 4294967295,
+                          "weight": 1,
+                          "actions": [{"type": "OUTPUT",
+                                       "port": 50}]}]}
+        ],
+        10925425496585651080: [
+            {"dpid": 10925425496585651080,
+             "type": "FF",
+             "group_id": 1,
+             "buckets": [{"watch_port": 53,
+                          "watch_group": 4294967295,
+                          "weight": 1,
+                          "actions": [{"type": "OUTPUT",
+                                       "port": 53}]},
+                         {"watch_port": 51,
+                          "watch_group": 4294967295,
+                          "weight": 1,
+                          "actions": [{"type": "OUTPUT",
+                                       "port": 51}]}]},
+            {"dpid": 10925425496585651080,
+             "type": "FF",
+             "group_id": 2,
+             "buckets": [{"watch_port": 53,
+                          "watch_group": 4294967295,
+                          "weight": 1,
+                          "actions": [{"type": "OUTPUT",
+                                       "port": 53}]},
+                         {"watch_port": 51,
+                          "watch_group": 4294967295,
+                          "weight": 1,
+                          "actions": [{"type": "OUTPUT",
+                                       "port": 51}]}]}
+        ]
     }
