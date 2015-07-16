0632741356b9b3a8661098cd04c9e7f282edb676
Rémy Lapeyrade <remy@lapeyrade.net>
2015-07-16 17:49:50 +0200

    Add some comments

--- Statistics/forge.py
+++ Statistics/forge.py
@@ -7,7 +7,8 @@ from database.models import Flux, Stats, Switch
 
 class forgeData(object):
 
-    probe_time_interval = 5
+    # delta time time between first and last stat value of the same request
+    time_interval = 5
     time_first_value = now()
 
     def get_data(self, source, destination, flux_type, period, unit):
@@ -15,22 +16,30 @@ class forgeData(object):
         pk = self.get_flux_id(source, destination, flux_type)
         switches = Switch.objects.all()
         datas_non_aggregated = []
+        # get statistics per switch (sum on same time values)
         for switch in switches:
             query = self.forge_query(pk, switch.pk, period)
             data = self.forge_data(query, unit)
             datas_non_aggregated.extend(data)
 
+        # only one flux to extract => no need for supplementary aggregations
         if pk.count() == 1:
             return datas_non_aggregated
 
+        # sort all values in chronological period
         datas_non_aggregated.sort(key=lambda stat: stat.get("time"))
+
         datas_aggregated = []
+
+        # set cursor on the earliest stat (done in forge_data)
         time_cursor = self.time_first_value
         value = 0
         for data in datas_non_aggregated:
+            # if the data is in the time interval, add the value
             if ((data["time"] - time_cursor) >= timedelta(seconds=0)) &\
-                    ((data["time"] - time_cursor) <= timedelta(seconds=self.probe_time_interval)):
+                    ((data["time"] - time_cursor) <= timedelta(seconds=self.time_interval)):
                 value += data["value"]
+            # else, register the aggregated data and move the cursor
             else:
                 datas_aggregated.append({
                     "value": value,
@@ -43,9 +52,10 @@ class forgeData(object):
     def get_flux_id(self, source="0", destination="0", flux_type="IPv4"):
 
         query = Flux.objects.all()
-
+        # flux types with no sources have null value on source column
         if flux_type == "ICMPv6" or flux_type == "ARP":
             query = query.filter(hote_src_id__isnull=True)
+
         elif source is not "0":
             query = query.filter(hote_src_id=source)
 
@@ -60,30 +70,37 @@ class forgeData(object):
         liste_pk = []
         for primary in pk:
             liste_pk.append(primary.get('idflux'))
+        # filter stats by flux and switch origin
         query = Stats.objects.filter(idflux__in=liste_pk).filter(idswitch_id=dpid)
 
         query = query.filter(time__gte=self.get_start_time(period))
 
+        # needed for conversion into average value
         query = query.order_by("time")
 
+        # sum on all the flux with the same time
         query = query.values("time").annotate(bytes=Sum('bytes'), packets=Sum('packets'))
 
         return query
 
     def forge_data(self, stats, unit='bytes'):
         data_list = []
+        # create a special list for retrieve the next value
         for index, stat in list(enumerate(stats)):
             try:
                 next = stats[index + 1]
             except IndexError:
-                # No more values
+                # No more values in the list
                 break
 
+            # average value in an time interval (second based)
             value = ((next.get(unit) - stat.get(unit)) / self.diff_seconds(next.get('time'), stat.get('time')))
+
             data = {'time': next.get('time'),
                     'value': int(value)}
             data_list.append(data)
 
+            # update the earliest value in the data
             if next["time"] < self.time_first_value:
                 self.time_first_value = next["time"]
 
@@ -100,7 +117,7 @@ class forgeData(object):
         if period == "hour":
             return self.now - timedelta(hours=1)
         elif period == "day":
-           return self.now - timedelta(days=1)
+            return self.now - timedelta(days=1)
         elif period == "month":
             return self.now - timedelta(days=31)
         elif period == "year":
