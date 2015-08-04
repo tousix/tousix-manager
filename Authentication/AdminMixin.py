ca2166acd41aa22db038f297b37e2cf626f81ee8
Rémy Lapeyrade <remy@lapeyrade.net>
2015-08-04 11:09:25 +0200

    Change admin verification method

--- Authentication/AdminMixin.py
+++ Authentication/AdminMixin.py
@@ -1,14 +1,13 @@
 # -*- coding: utf8 -*-
 __author__ = 'remy'
 
-from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import Http404
 
 
 class AdminVerificationMixin(object):
 
-    def admin_check(user):
-        return user.is_superuser
-
-    @user_passes_test(admin_check, login_url=None, redirect_field_name=None)
     def dispatch(self, request, *args, **kwargs):
-        return super(AdminVerificationMixin, self).dispatch(request, *args, **kwargs)
+        if request.user.is_superuser:
+            return super(AdminVerificationMixin, self).dispatch(request, *args, **kwargs)
+        else:
+            raise Http404()
