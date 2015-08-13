7746f303711defe84b3a3dca5f834d9b5e006b07
Rémy Lapeyrade <remy@lapeyrade.net>
2015-08-13 14:13:28 +0200

    Add registration panel in admin view

--- admin_TouSIX/admin.py
+++ admin_TouSIX/admin.py
@@ -6,6 +6,7 @@ from admin_TouSIX.actions import generate_routeserver_conf, generate_openflow_ru
 from fsm_admin.mixins import FSMTransitionMixin
 from admin_TouSIX.adminsite import admin_tousix
 from django.contrib.auth.admin import UserAdmin
+from registration.admin import RegistrationAdmin
 # Register your models here.
 
 
